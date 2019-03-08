473b040ab0d7eaddbb8b655d188de617433ef659
GMJJ <gmjj.peterson@gmail.com>
2019-03-07 19:43:37 -0600

    Changed pointers & references in Pixy2 Test Bench based on Arduino code

--- src/main/cpp/pixy2/LineFeatures.cpp
+++ src/main/cpp/pixy2/LineFeatures.cpp
@@ -1,27 +1,23 @@
 #include "pixy2/LineFeatures.h"
 
-LineFeatures::LineFeatures()
-{
-}
-
-LineFeatures::LineFeatures(std::vector<Vector*> &vectors, std::vector<Intersection*> &intersections, std::vector<Barcode*> &barcodes)
+LineFeatures::LineFeatures(Vector *vectors, Intersection *intersections, Barcode *barcodes)
 {
 	_vectors = vectors;
 	_intersections = intersections;
 	_barcodes = barcodes;
 }
 
-std::vector<Barcode*> LineFeatures::Barcodes()
+Barcode* LineFeatures::Barcodes()
 {
 	return _barcodes;
 }
 
-std::vector<Intersection*> LineFeatures::Intersections()
+Intersection* LineFeatures::Intersections()
 {
 	return _intersections;
 }
 
-std::vector<Vector*> LineFeatures::Vectors()
+Vector* LineFeatures::Vectors()
 {
 	return _vectors;
 }
